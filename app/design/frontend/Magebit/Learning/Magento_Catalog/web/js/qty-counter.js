411c744e39a1a953ac85f9adce04c90842b3ee92
Sergejs Makarovs <sergejs.makarovs@magebit.com>
2024-09-20 13:21:21 +0300

    delete ml01

--- app/design/frontend/Magebit/Learning/Magento_Catalog/web/js/qty-counter.js
+++
@@ -1,9 +0,0 @@
-
-function subtract() {
-    document.getElementById("qty").value = Math.max(1, +document.getElementById("qty").value - 1);
-}
-
-function add() {
-    document.getElementById("qty").value = Math.min(+document.getElementById("qty").max, +document.getElementById("qty").value + 1);
-}
-
